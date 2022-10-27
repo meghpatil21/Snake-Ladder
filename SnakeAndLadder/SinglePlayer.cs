8dac251cea7fbc8eb00d77586f1986d905824ab7
meghpatil21 <megh.after18@gmail.com>
2022-10-27 21:09:42 +0530

    Implemented UC3

--- SnakeAndLadder/SinglePlayer.cs
+++ SnakeAndLadder/SinglePlayer.cs
@@ -14,14 +14,36 @@ namespace SnakeAndLadder
             Console.WriteLine("The Game Begins!");
             Console.WriteLine("Player Position : 0");
 
-            int startPosiion = 0;
+            int startPosition = 0;
+            int position = 0;
 
             Random random = new Random();
             int dice = random.Next(1, 7);
 
             Console.WriteLine();
             Console.WriteLine("Dice has been Rolled: " + dice);
+            Console.WriteLine("Checking for Options:");
+
+            int opt = random.Next(1, 4);
+            switch (opt)
+            {
+                case 1:
+                    Console.WriteLine("It's No Play: You don't advance");
+                    break;
+                case 2:
+                    Console.WriteLine("Congrats, its Ladder: You advance by " + dice);
+                    position = position + dice;
+                    break;
+                case 3:
+                    Console.WriteLine("Opps!,its a Snake: You retreat by " + dice);
+                    position = position - dice;
+                    break;
+
+
+            }
 
         }
+
     }
+
 }
